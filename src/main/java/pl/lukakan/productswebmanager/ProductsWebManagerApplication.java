8529ca25e80874425b6007dcecabdf5cc00b4f2c
Lukasz K <kanicki.lukasz@gmail.com>
2021-03-28 14:43:45 +0200

    initial commit

---
+++ src/main/java/pl/lukakan/productswebmanager/ProductsWebManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.lukakan.productswebmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsWebManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsWebManagerApplication.class, args);
+    }
+
+}
