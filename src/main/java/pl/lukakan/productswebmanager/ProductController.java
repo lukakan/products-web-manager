259bc773ebdc0d2fb75654541511bf9b579a7025
Lukasz K <kanicki.lukasz@gmail.com>
2021-04-03 11:46:28 +0200

    thymeleaf fragments and layouts added.

--- src/main/java/pl/lukakan/productswebmanager/ProductController.java
+++ src/main/java/pl/lukakan/productswebmanager/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 
 @Controller
@@ -16,6 +17,11 @@ public class ProductController {
     @Autowired
     public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
     }
 
     @GetMapping("/list")
